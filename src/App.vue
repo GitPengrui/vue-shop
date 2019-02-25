c39af6f65473d93fa59a6dd2b12ceb1bbe563d15
jiayingxuu <18164012171@163.com>
2019-02-25 11:21:46 +0800

    1111

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <router-link to="/about">About</router-link>
       <h1>111111111111111</h1>
       <h2>2222222222222</h2>
+      <h1>222222222222</h1>
     </div>
     <router-view/>
   </div>
